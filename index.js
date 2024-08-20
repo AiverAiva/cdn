91d400ba9ba6a9ebb635a4f19365c7b44bda51af
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-08-21 02:13:08 +0800

    feat: make folders always on top of the list

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 const fs = require('fs');
 const path = require('path');
-
+ 
 const rootDir = 'src/'
-
+ 
 function generateFileTree(dir) {
   const files = fs.readdirSync(dir);
-
+ 
   let fileTree = '<ul>';
-  files.forEach((file) => {
+  const directories = files.filter((file) => fs.statSync(path.join(dir, file)).isDirectory());
+  const filesOnly = files.filter((file) => !fs.statSync(path.join(dir, file)).isDirectory());
+ 
+  directories.forEach((file) => {
     const fullPath = path.join(dir, file);
     const relativePath = path.relative(rootDir, fullPath);
-    const isDirectory = fs.statSync(fullPath).isDirectory();
-
-    if (isDirectory) {
-      fileTree += `<li><details><summary><i class="fas fa-folder"></i> ${file}</summary>`;
-      fileTree += generateFileTree(fullPath);
-      fileTree += '</details></li>';
-    } else {
-      fileTree += `<li><a href="${rootDir + relativePath}"><i class="fas fa-file"></i> ${file}</a></li>`;
-    }
+    fileTree += `<li><details><summary><i class="fas fa-folder"></i> ${file}</summary>`;
+    fileTree += generateFileTree(fullPath);
+    fileTree += '</details></li>';
   });
+ 
+  filesOnly.forEach((file) => {
+    const fullPath = path.join(dir, file);
+    const relativePath = path.relative(rootDir, fullPath);
+    fileTree += `<li><a href="${rootDir + relativePath}"><i class="fas fa-file"></i> ${file}</a></li>`;
+  });
+ 
   fileTree += '</ul>';
   return fileTree;
 }
-
+ 
 const fileTreeHTML = `
 <!DOCTYPE html>
 <html>
@@ -38,6 +42,6 @@ const fileTreeHTML = `
 </body>
 </html>
 `;
-
+ 
 fs.writeFileSync('index.html', fileTreeHTML);
 console.log('File tree generated and saved as "index.html"');
